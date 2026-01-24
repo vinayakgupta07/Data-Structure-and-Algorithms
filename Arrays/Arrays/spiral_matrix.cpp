881da9c7052312eb574a3432c4351bda095de27b
vinayakgupta07 <spiral076@gmail.com>
2026-01-24 19:06:20 +0530

    print the matrix in spiral matrix

---
+++ Arrays/Arrays/spiral_matrix.cpp
@@ -0,0 +1,52 @@
+# spiral matrix
+
+// ⏱️ Complexity
+//     Time	O(n)
+//     Space	O(n)
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> ans;
+        if (matrix.empty()) return ans;
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int top = 0, bottom = m - 1;
+        int left = 0, right = n - 1;
+
+        while (top <= bottom && left <= right) {
+
+            // left → right
+            for (int i = left; i <= right; i++) {
+                ans.push_back(matrix[top][i]);
+            }
+            top++;
+
+            // top → bottom
+            for (int i = top; i <= bottom; i++) {
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+
+            // right → left
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    ans.push_back(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            // bottom → top
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    ans.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        return ans;
+    }
+};
