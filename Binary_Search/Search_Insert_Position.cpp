41761c02b1dc5b97ec2207884bd15c435a746886
vinayakgupta07 <spiral076@gmail.com>
2026-01-27 21:40:44 +0530

    implement search insert position

---
+++ Binary_Search/Search_Insert_Position.cpp
@@ -0,0 +1,26 @@
+# Search Insert Position
+
+// ⏱️Complexity
+//    time:O(log n)
+//    space:O(1)
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+      int n=nums.size();
+      int low=0;
+      int high=n-1;
+      while(low<=high){
+        int mid=(high+low)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        else if(nums[mid]<target){
+            low=mid+1;
+        }else{
+            high=mid-1;
+        }
+      }  
+      return low;
+    }
+};
