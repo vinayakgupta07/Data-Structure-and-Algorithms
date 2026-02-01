2a8edef58e551200ef7c3e33126e52e89815bb5c
vinayakgupta07 <spiral076@gmail.com>
2026-02-01 23:14:10 +0530

    implement single element in a sorted array

---
+++ Binary_Search/single_element.cpp
@@ -0,0 +1,26 @@
+# Single Element in a Sorted Array
+
+// Time & Space Complexity
+//     Time: O(log n)
+//     Space: O(1)
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int low = 0, high = nums.size() - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            // make mid even
+            if (mid % 2 == 1) mid--;
+
+            if (nums[mid] == nums[mid + 1]) {
+                low = mid + 2;
+            } else {
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
+};
