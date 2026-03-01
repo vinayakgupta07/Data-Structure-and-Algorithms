d0ec112af398ec883d0f3a3fd9f335fb5e87b41a
vinayakgupta07 <spiral076@gmail.com>
2026-03-01 21:45:44 +0530

    implement roman to string

---
+++ string/roman_to_integer.cpp
@@ -0,0 +1,30 @@
+#Roman to integer
+
+// ⏱ Time Complexity
+// O(n)
+// 💾 Space Complexity
+// O(1)
+  
+class Solution {
+public:
+    int romanToInt(string s) {
+          unordered_map<char, int> mp = {
+            {'I',1}, {'V',5}, {'X',10},
+            {'L',50}, {'C',100},
+            {'D',500}, {'M',1000}
+        };
+        
+        int total = 0;
+        
+        for(int i = 0; i < s.length(); i++) {
+            
+            if(i + 1 < s.length() && mp[s[i]] < mp[s[i+1]]) {
+                total -= mp[s[i]];
+            } else {
+                total += mp[s[i]];
+            }
+        }
+        
+        return total;
+    }
+};
