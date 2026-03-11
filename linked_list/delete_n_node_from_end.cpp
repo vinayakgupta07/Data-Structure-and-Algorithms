1b31f5621ce63782e7ffd5856f730cea2f29a195
vinayakgupta07 <spiral076@gmail.com>
2026-03-11 18:10:45 +0530

    remove nth node from end algorithm

---
+++ linked_list/delete_n_node_from_end.cpp
@@ -0,0 +1,42 @@
+#Remove Nth Node From End of List
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+  
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+         ListNode* fast = head;
+        ListNode* slow = head;
+        
+        for(int i = 0; i < n; i++) {
+            fast = fast->next;
+        }
+
+        
+        if(fast == NULL) {
+            return head->next;
+        }
+
+        
+        while(fast->next != NULL) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        
+        slow->next = slow->next->next;
+
+        return head;
+    }
+};
