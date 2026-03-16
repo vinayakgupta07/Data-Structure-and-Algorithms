a377cf5aea09a7befec761c90b1c682953fb71a2
vinayakgupta07 <spiral076@gmail.com>
2026-03-16 21:29:07 +0530

    Implement intersection of two linked lists
    Added implementation to find the intersection of two linked lists.

---
+++ linked_list/intersection_0f_two_LL.cpp
@@ -0,0 +1,34 @@
+#Intersection of Two Linked Lists
+// ⏱ Complexity
+// Time	O(n + m)
+// Space	O(1)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+               ListNode* a = headA;
+          ListNode* b = headB;
+
+        while(a != b) {
+            if(a == NULL) 
+                a = headB;
+            else 
+                a = a->next;
+
+            if(b == NULL) 
+                b = headA;
+            else 
+                b = b->next;
+        }
+
+        return a;
+    }
+};
