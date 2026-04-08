b01e55bb868da4389c1821d2cbed32579079e0d2
vinayakgupta07 <spiral076@gmail.com>
2026-04-08 23:37:30 +0530

    Implement algorithm to check palindrome of sentence

---
+++ string/Palindrome_sentence.cpp
@@ -0,0 +1,24 @@
+#valid palindrome 
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            // Skip non-alphanumeric characters
+            while (left < right && !isalnum(s[left])) left++;
+            while (left < right && !isalnum(s[right])) right--;
+
+            // Compare characters (case-insensitive)
+            if (tolower(s[left]) != tolower(s[right])) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
