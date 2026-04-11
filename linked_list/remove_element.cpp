a39fa0a6f4162aa457bfde69d7f51b935e44fc22
vinayakgupta07 <spiral076@gmail.com>
2026-04-11 23:26:24 +0530

    algorithm for remove element from linked list

---
+++ linked_list/remove_element.cpp
@@ -0,0 +1,32 @@
+#Remove Linked List Elements
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+      while (head != nullptr && head->val == val) {
+            head = head->next;
+        }
+
+        ListNode* temp = head;
+
+        while (temp != nullptr && temp->next != nullptr) {
+            if (temp->next->val == val) {
+                temp->next = temp->next->next;
+            } else {
+                temp = temp->next;
+            }
+        }
+
+        return head;
+    }
+};
