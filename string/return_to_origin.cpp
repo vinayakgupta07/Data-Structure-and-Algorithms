581737eef112082da177e5181f11e691d5fd26c7
vinayakgupta07 <spiral076@gmail.com>
2026-04-12 14:59:38 +0530

    Robot Return to Origin implementation

---
+++ string/return_to_origin.cpp
@@ -0,0 +1,18 @@
+#Robot Return to Origin
+
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+       
+        int x = 0, y = 0;
+        
+        for (char move : moves) {
+            if (move == 'U') y++;
+            else if (move == 'D') y--;
+            else if (move == 'L') x--;
+            else if (move == 'R') x++;
+        }
+        
+        return (x == 0 && y == 0);
+    }
+};
