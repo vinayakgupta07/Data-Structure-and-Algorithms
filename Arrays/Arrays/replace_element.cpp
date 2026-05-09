3d8ad6ea5295e66a6a107ae3d9a5d2cf67f23b69
vinayakgupta07 <spiral076@gmail.com>
2026-05-09 16:39:49 +0530

    Add Replace Elements with Greatest Element on Right Side solution
    Implement a solution to replace each element with the greatest element on its right side in a vector.

---
+++ Arrays/Arrays/replace_element.cpp
@@ -0,0 +1,16 @@
+#Replace Elements with Greatest Element on Right Side
+
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+       int maxi = -1;
+
+        for(int i = arr.size() - 1; i >= 0; i--) {
+            int temp = arr[i];
+            arr[i] = maxi;
+            maxi = max(maxi, temp);
+        }
+
+        return arr;  
+    }
+};
