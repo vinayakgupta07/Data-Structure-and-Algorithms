156d58e2d22039b3fb1657dd3f69ee50985ebcce
vinayakgupta07 <spiral076@gmail.com>
2026-05-16 18:17:05 +0530

    Add function to determine if array is good
    Implement a function to check if an array is good based on its elements.

---
+++ Arrays/Arrays/array_is_good.cpp
@@ -0,0 +1,17 @@
+#Check if Array is Good
+
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] != i + 1)
+                return false;
+        }
+
+        return nums[n - 1] == n - 1;
+    }
+};
