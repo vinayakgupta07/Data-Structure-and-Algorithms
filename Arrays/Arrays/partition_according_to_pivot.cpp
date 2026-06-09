89786915683b3a7d8fb930d9c66e3753f01092ad
vinayakgupta07 <spiral076@gmail.com>
2026-06-09 16:34:15 +0530

    Add algorithm for partition according to pivot

---
+++ Arrays/Arrays/partition_according_to_pivot.cpp
@@ -0,0 +1,38 @@
+#Partition Array According to Given Pivot
+
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int> small;
+        vector<int> equal;
+        vector<int> large;
+
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]< pivot){
+                small.push_back(nums[i]);
+            }
+            else if(nums[i]== pivot){
+                equal.push_back(nums[i]);
+            }
+            else{
+                 large.push_back(nums[i]);
+            }
+        }
+
+       vector<int> ans;
+
+       for(int i=0;i<small.size();i++){
+        ans.push_back(small[i]);
+       }
+
+       for(int i=0;i<equal.size();i++){
+        ans.push_back(equal[i]);
+       }
+
+       for(int i=0;i<large.size();i++){
+        ans.push_back(large[i]);
+       }
+    return ans;
+    }
+};
