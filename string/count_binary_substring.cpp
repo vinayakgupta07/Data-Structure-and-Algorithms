491bb16077c27069a6e97ddaedaa5b37a34b3c08
vinayakgupta07 <spiral076@gmail.com>
2026-06-13 17:47:11 +0530

    add solution for count binary substring
    Implement a solution to count binary substrings in a given string.

---
+++ string/count_binary_substring.cpp
@@ -0,0 +1,23 @@
+#Count Binary Substrings
+
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+          int prev = 0;
+        int curr = 1;
+        int ans = 0;
+
+        for (int i = 1; i < s.size(); i++) {
+            if (s[i] == s[i - 1]) {
+                curr++;
+            } else {
+                ans += min(prev, curr);
+                prev = curr;
+                curr = 1;
+            }
+        }
+
+        ans += min(prev, curr);
+        return ans;
+    }
+};
