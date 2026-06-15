0ece067f8b8ba3e20952e5be3d5e76b44e1f5645
vinayakgupta07 <spiral076@gmail.com>
2026-06-15 16:46:35 +0530

    Implement search for Binary Search Tree

---
+++ tree/search_in_BST.cpp
@@ -0,0 +1,29 @@
+#Search in a Binary Search Tree
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while (root != nullptr) {
+            if (root->val == val)
+                return root;
+
+            if (val < root->val)
+                root = root->left;
+            else
+                root = root->right;
+        }
+
+        return nullptr;
+    }
+};
