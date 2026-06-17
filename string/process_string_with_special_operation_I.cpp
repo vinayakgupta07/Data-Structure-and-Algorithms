b1d9fd960cf38d6a4f6e8c86c57dcd3d6df74581
vinayakgupta07 <spiral076@gmail.com>
2026-06-17 18:36:56 +0530

    Add solution for Process String with Special Operations I
    Implement string processing with special operations such as removing last character, duplicating, and reversing.

---
+++ string/process_string_with_special_operation_I.cpp
@@ -0,0 +1,31 @@
+#Process String with Special Operations I
+
+class Solution {
+public:
+    string processStr(string s) {
+        string ans = "";
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s[i] >= 'a' && s[i] <= 'z') {
+                ans += s[i];
+            }
+
+            else if (s[i] == '*') {
+                if (ans.length() > 0) {
+                    ans.pop_back();
+                }
+            }
+
+            else if (s[i] == '#') {
+                ans += ans;
+            }
+
+            else if (s[i] == '%') {
+                reverse(ans.begin(), ans.end());
+            }
+        }
+
+        return ans;
+    }
+};
