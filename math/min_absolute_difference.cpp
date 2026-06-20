6480fb1c19d108a1f431d653c25e7b15fb60a578
vinayakgupta07 <spiral076@gmail.com>
2026-06-20 18:17:15 +0530

    add solution for Minimum Absolute Difference
    Add a new file for calculating minimum absolute difference

---
+++ math/min_absolute_difference.cpp
@@ -0,0 +1,25 @@
+#Minimum Absolute Difference
+
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+
+        sort(arr.begin(), arr.end());
+
+        int minDiff = INT_MAX;
+
+        for (int i = 1; i < arr.size(); i++) {
+            minDiff = min(minDiff, arr[i] - arr[i - 1]);
+        }
+
+        vector<vector<int>> ans;
+
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr[i] - arr[i - 1] == minDiff) {
+                ans.push_back({arr[i - 1], arr[i]});
+            }
+        }
+
+        return ans;
+    }
+};
