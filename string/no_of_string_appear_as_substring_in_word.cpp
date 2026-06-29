7acaa6bd8552cd2a726dc3310989939f09719913
vinayakgupta07 <spiral076@gmail.com>
2026-06-29 17:41:48 +0530

    add solution to count Number of Strings That Appear as Substrings in Word

---
+++ string/no_of_string_appear_as_substring_in_word.cpp
@@ -0,0 +1,16 @@
+#Number of Strings That Appear as Substrings in Word
+
+class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        int count = 0;
+
+        for (int i = 0; i < patterns.size(); i++) {
+            if (word.find(patterns[i]) != -1) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
