b716f64cf65b9108885c2da943a90c768afd86dd
vinayakgupta07 <spiral076@gmail.com>
2026-07-09 20:15:26 +0530

    add solution to convert string to lower case

---
+++ string/to_lower.cpp
@@ -0,0 +1,11 @@
+#To Lower Case
+
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(int i=0;i<s.length();i++){
+s[i]=tolower(s[i]);
+        }
+        return s;
+    }
+};
