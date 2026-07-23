ca2e9612e89a482fa14ad5a83e502859ab853bfb
vinayakgupta07 <spiral076@gmail.com>
2026-07-23 18:16:45 +0530

    Implement solution to check if two arrays can be equal

---
+++ Arrays/Arrays/make_two_array_equal.cpp
@@ -0,0 +1,16 @@
+#Make Two Arrays Equal by Reversing Subarrays
+
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        sort(target.begin(), target.end());
+        sort(arr.begin(), arr.end());
+
+        for (int i = 0; i < target.size(); i++) {
+            if (target[i] != arr[i])
+                return false;
+        }
+
+        return true;
+    }
+};
